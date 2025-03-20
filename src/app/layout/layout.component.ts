e0167823215a527e39c30692b655d07f0c997b5e
Cedric9681 <20229681@s.ubaguio.edu>
2025-03-20 15:22:54 +0800

    third commit

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [HeaderComponent, SidebarComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
